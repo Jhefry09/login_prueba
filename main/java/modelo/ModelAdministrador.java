4f1d9b3e179a63d1d29151eb24cf860b94bbb32e
Jhefry09 <jhefrycabanillas@gmail.com>
2025-06-22 15:31:24 -0500

    edicion para poner mayuscula a la primera letra de el archivo administradores.java en entidad, y cambio de variables en el model

--- main/java/modelo/ModelAdministrador.java
+++ main/java/modelo/ModelAdministrador.java
@@ -4,12 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import entidad.administradores;
+import entidad.Administradores;
 import utils.MySQLDBConexion;
 
 public class ModelAdministrador {
-public administradores IniciarSesion(String usuario, String clave) {
-	administradores admin = null;
+public Administradores IniciarSesion(String usuario, String clave) {
+	Administradores admin = null;
 	try {
 		Connection cn = MySQLDBConexion.getConexion();
 		String sql = "SELECT * FROM administradores WHERE usuario_admin=? AND clave_admin=?";
@@ -18,7 +18,7 @@ public administradores IniciarSesion(String usuario, String clave) {
 		ps.setString(2, clave);
 		ResultSet rs = ps.executeQuery();
 		if (rs.next()) {
-			admin = new administradores();
+			admin = new Administradores();
 			admin.setId_admin(rs.getInt("id_admin"));
 			admin.setNombre_admin(rs.getString("nombre_admin"));
 			admin.setApellidos_admin(rs.getString("apellidos_admin"));
