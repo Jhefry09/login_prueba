38db60d6bddafe0f3279c6521cf6704ca48b7f6c
Jhefry09 <jhefrycabanillas@gmail.com>
2025-06-22 15:32:06 -0500

    edicion para poner mayuscula a la primera letra de el archivo administradores.java en entidad

---
+++ main/java/entidad/Administradores.java
@@ -0,0 +1,37 @@
+package entidad;
+
+public class Administradores {
+private int id_admin;
+private String nombre_admin, apellidos_admin, usuario_admin, clave_admin;
+public Administradores() {
+}
+public int getId_admin() {
+	return id_admin;
+}
+public void setId_admin(int id_admin) {
+	this.id_admin = id_admin;
+}
+public String getNombre_admin() {
+	return nombre_admin;
+}
+public void setNombre_admin(String nombre_admin) {
+	this.nombre_admin = nombre_admin;
+}
+public String getApellidos_admin() {
+	return apellidos_admin;
+}
+public void setApellidos_admin(String apellidos_admin) {
+	this.apellidos_admin = apellidos_admin;
+}
+public String getUsuario_admin() {
+	return usuario_admin;
+}
+public void setUsuario_admin(String usuario_admin) {
+	this.usuario_admin = usuario_admin;
+}
+public String getClave_admin() {
+	return clave_admin;
+}
+public void setClave_admin(String clave_admin) {
+	this.clave_admin = clave_admin;
+}}
